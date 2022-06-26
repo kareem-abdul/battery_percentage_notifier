41ca329ae80ee014d8bfcb78c9308355e4852ab4
kareem-abdul <abdul.k@flycatchtech.com>
2022-06-26 20:48:45 +0530

    feat: removed the infinite while loop

--- notifier.sh
+++ notifier.sh
@@ -1,32 +1,16 @@
 #!/bin/bash
 
-MAX_PERCENTAGE=80
-MIN_PERCENTAGE=20
+MAX_PERCENTAGE=75
+SOUND='/usr/share/sounds/freedesktop/stereo/bell.oga'
 
 main() {
-	while true
-	do
-		local info=$(upower -i $(upower -e | grep 'BAT'))
-		local state=$(grep "state" <<< $info | sed -E 's/.*\s(\w+?)/\1/g')
-		local percentage=$(grep "percentage" <<< $info | sed -E 's/.*?\s([0-9]+?)%/\1/g')
-		if [ "$state" = "charging" ]
-		then
-			if [ $percentage -gt $MAX_PERCENTAGE ] 
-			then
-				notify-send -t 1000 -u critical "Battery above $MAX_PERCENTAGE%" "Chargin: $percentage%"
-				paplay /usr/share/sounds/freedesktop/stereo/bell.oga
-			fi
-		elif [ "$state" = "discharging" ]
-		then
-			if [ $percentage -lt $MIN_PERCENTAGE ]
-			then
-				notify-send "Battery bellow $MIN_PERCENTAGE%!" "discharging: $percentage%"
-				paplay /usr/share/sounds/freedesktop/stereo/bell.oga
-			fi
-		fi
-		sleep 240
-	done
+	local info=`upower -i $(upower -e | grep 'BAT' | head -n 1)`;
+	local state=`grep state <<< $info | cut -d: -f2 | tr -d ' '`;
+	local percentage=`grep percentage <<< $info | cut -d: -f2 | tr -d ' ' | cut -d% -f1`;
 
+	if [ "$state" = "charging" ]; then
+		[ $percentage -gt $MAX_PERCENTAGE ] && (notify-send -a "Battery Percentage" "Batter above $MAX_PERCENTAGE%" && paplay $SOUND);
+	fi
 }
 
 main
